b22d9e52adfb126c3ebf7fadbbd0a3e5ff619511
Piotr S <psiemb@tlen.pl>
2020-10-29 18:57:18 +0100

    Interfejsy - streamowanie po Typ.

--- src/main/java/interfejsy/budowaFinanse/Ksiegowa.java
+++ src/main/java/interfejsy/budowaFinanse/Ksiegowa.java
@@ -1,12 +1,25 @@
 package interfejsy.budowaFinanse;
 
+import interfejsy.budowaFinanse.dokument.TypDokumentu;
+
 import java.util.List;
 
 public class Ksiegowa {
 
     public double oblicz(List<DokumentFinansowy> dokumentyFinansowe) {
-        return dokumentyFinansowe.stream()
+
+        Double koszt = dokumentyFinansowe.stream()
+                .filter(dok -> TypDokumentu.KARA.equals(dok.getTyp()) || TypDokumentu.FAKTURA_ZAKUPOWA.equals(dok.getTyp()))
                 .map(DokumentFinansowy::getWartosc)
                 .reduce(0.0, Double::sum);
+
+        Double zysk = dokumentyFinansowe.stream()
+                .filter(dok -> TypDokumentu.FAKTURA_SPRZEDAZOWA.equals(dok.getTyp()))
+                .map(DokumentFinansowy::getWartosc)
+                .reduce(0.0, Double::sum);
+
+        return zysk - koszt;
+
+
     }
 }
--- src/main/java/interfejsy/budowaFinanse/dokument/FakturaSprzedazowa.java
+++ src/main/java/interfejsy/budowaFinanse/dokument/FakturaSprzedazowa.java
@@ -1,10 +1,43 @@
 package interfejsy.budowaFinanse.dokument;
 
-public class FakturaSprzedazowa {
+import interfejsy.budowaFinanse.DokumentFinansowy;
+
+public class FakturaSprzedazowa implements DokumentFinansowy {
 
     private TypDokumentu typDokumentu;
     private double wartosc;
     private String komuSprzedajemy;
+
+    public FakturaSprzedazowa(TypDokumentu typDokumentu, double wartosc, String komuSprzedajemy) {
+        this.typDokumentu = typDokumentu;
+        this.wartosc = wartosc;
+        this.komuSprzedajemy = komuSprzedajemy;
+    }
+    @Override
+    public TypDokumentu getTyp() {
+        return typDokumentu;
+    }
+
+    public void setTypDokumentu(TypDokumentu typDokumentu) {
+        this.typDokumentu = typDokumentu;
+    }
+
+    @Override
+    public double getWartosc() {
+        return wartosc;
+    }
+
+    public void setWartosc(double wartosc) {
+        this.wartosc = wartosc;
+    }
+
+    public String getKomuSprzedajemy() {
+        return komuSprzedajemy;
+    }
+
+    public void setKomuSprzedajemy(String komuSprzedajemy) {
+        this.komuSprzedajemy = komuSprzedajemy;
+    }
 
     //TODO: implements DokumentFinansowy
     //TODO: gettery / settery / construktor
